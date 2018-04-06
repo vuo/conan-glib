d2f5a8d840451c5aab486394e60322282893c037
Steve Mokris <smokris@softpixel.com>
2018-04-06 09:53:48 -0400

    Copy libraries to the lib folder.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,13 +7,13 @@ class GlibTestConan(ConanFile):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*.dylib', dst='bin', src='lib')
+        self.copy('*.dylib', src='lib', dst='lib')
 
     def test(self):
         self.run('qbs run')
 
         # Ensure we only link to system libraries and to libraries we built.
-        self.run('! (otool -L bin/libglib.dylib | tail +3 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+        self.run('! (otool -L lib/libglib.dylib | tail +3 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
 
         # Ensure we don't search any absolute rpaths.
-        self.run('! (otool -l bin/libglib.dylib | grep -A2 LC_RPATH | grep path | cut -b 15- | cut -d"(" -f1 | egrep -v "^[^/]")')
+        self.run('! (otool -l lib/libglib.dylib | grep -A2 LC_RPATH | grep path | cut -b 15- | cut -d"(" -f1 | egrep -v "^[^/]")')
